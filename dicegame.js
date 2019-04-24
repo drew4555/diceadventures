5a3532c56208620eb30111f370fe374484660b47
andrew carr <drew4555@yahoo.com>
2019-04-24 15:17:02 -0500

    reworked some functions and deleated a few global variables

--- dicegame.js
+++ dicegame.js
@@ -1,4 +1,4 @@
-function mainGamefunction(){
+function game(){
   //dice//
   var dice1 = { //characture selection
     sides: 4,
@@ -48,150 +48,157 @@ function mainGamefunction(){
       health: 100,
       power: 20,
     }
-     var finalboss = {
-      health: 100,
-      power: 40
+    var monster1 = {
+      health: 10,
+      power: 10,
+      trait: "goblin"
     }
+     var finalboss = {
+      health: 40,
+      power: 40,
+      trait: "Dragon"
 
-    var currentPlayerHealth;
-    var currentMonsterHealth;
-    var playerAttack;
-
-    //call functions in logical order
+    }
+    var monster2 = {
+      health: 20,
+      power: 15,
+      trait: "Bridge Troll"
+    }
+    var monster3 = {
+      health: 30,
+      power: 20,
+      trait: "Slime"
+    }
+   let totalSpaces = 30;
+    let currentPosition = 1;
     CharSelect();
-    move();
-
-    randEnc();
+    while (totalSpaces > currentPosition){
+      move();
+      randEnc();
+    
 
   }    
+  
+ 
+  
+  
+    
+
+  
+    
+    
+    
+    
+
     //functions
-      function battle(currentMonster){
-        let playerAttack = dice3.roll();
-        let playerDefense = dice4.roll();
-        let monsterAttack = dice3.roll();
-        let monsterDefense = dice4.roll();
-          if (playerAttack > monsterDefense){
-            currentMonster.health -=(playerAttack-monsterDefense);
-          }
-          else{
-            console.log("you did no damage");
-          }
-          if (monsterAttack > playerDefense){
-            player1.health -=(monsterAttack-playerDefense);
-          }
-          else{
-          console.log('the monster did no damgae');
-      }
+function battle(currentMonster){
+
+  while(player1.health > 0 && currentMonster.health > 0){
+    console.log("Current Player Health" + " " + player1.health);
+    console.log("Current Monster Health" + " " + currentMonster.health);
+    let playerAttack = dice3.roll();
+    let playerDefense = dice4.roll();
+    let monsterAttack = dice3.roll();
+    let monsterDefense = dice4.roll();
+    if (playerAttack > monsterDefense){
+      currentMonster.health -= (playerAttack-monsterDefense);
+      console.log("Current Player Health" + " " + player1.health)
     }
-      function CharSelect(){
-        let result = dice1.roll();
-          console.log (result);
-            var imagefilepath;
-              if (result === (1)){
-                imagefilepath = "brutal-helm.svg";
-              }
-              else if (result === (2)){
-                imagefilepath = "cultist.svg";
-              }
-              else if (result === (3)){
-                imagefilepath = "elf-helmet.svg";
-              }
-              else if (result === (4)){
-                imagefilepath = "dwarf-helmet.svg";
-              }
-                document.getElementById("image").src = imagefilepath;
-                  console.log (imagefilepath); 
-      }
+    else{
+      console.log("you did no damage");
+    }
+    if (monsterAttack > playerDefense){
+      player1.health -=(monsterAttack-playerDefense);
+      console.log("Current monster Health" + " " + currentMonster.health);
+    }
+    else{
+    console.log('the monster did no damage');
+    }
+    if (player1.health <= 0){
+      
+      console.log ("Game Over");
+      break;
+    }
+    else if(finalboss.health <= 0){
+      console.log("Congratulations You Beat the Final Boss");
+      break;
+    }
+  }
+  }
+function CharSelect(){
+  let result = dice1.roll();
+  var imagefilepath;
+  if (result === (1)){
+    imagefilepath = "brutal-helm.svg";
+  }
+  else if (result === (2)){
+    imagefilepath = "cultist.svg";
+  }
+  else if (result === (3)){
+    imagefilepath = "elf-helmet.svg";
+  }
+  else if (result === (4)){
+    imagefilepath = "dwarf-helmet.svg";
+  }
+  document.getElementById("image").src = imagefilepath;
+  console.log (imagefilepath); 
+}
 
 
 function move(){
-let movement = dice2.roll();
-console.log (movement);
-console.log ("characture moves" + " " + movement + " " + "spaces");
+  console.log ("Currently at space" + " " + currentPosition);  
+  currentPosition = dice2.roll() + currentPosition;
+  console.log ("characture moves" + " " + dice2.roll() + " " + "spaces");
+
+  if (currentPosition >= totalSpaces){
+    console.log("Final Boss!")
+    battle(finalboss);
+    
+  }
 }
-dice2.roll();
+
+
 
 function randEnc(){
+
+// if(currentPosition >= fina)
+// {
+//   sdlfasldkfj
+// }
+// else{
+
+
 let randomEncounter = dice6.roll();
-console.log (randomEncounter);
 if (randomEncounter == 1 || randomEncounter == 3 || randomEncounter == 5 ||
   randomEncounter == 7){
 
- battle(monster1); console.log (monster1);
-  var monster1 = {
-  health: 30,
-  power: 10,
-} 
-  }
+ battle(monster1); console.log ("goblin");
+ 
+}
   else if (randomEncounter == 9 || randomEncounter == 11 || randomEncounter == 13 ||
     randomEncounter == 15){
-    battle(monster2); console.log(monster2);
-    var monster2 = {
-      health: 40,
-      power: 15,
-    }
+    battle(monster2); console.log("Bridge Troll");
+    
+  
   } 
     else if (randomEncounter == 17 || randomEncounter == 19 || randomEncounter == 20){
-      battle(monster3); console.log (monster3);
-      var monster3 = {
-        health: 50,
-        power: 20,
-      }
+      battle(monster3); console.log ("slime");
+      
     }
       else if (randomEncounter == 2 || randomEncounter == 4 || randomEncounter == 6 || randomEncounter == 8
         || randomEncounter == 10 || randomEncounter == 12 || randomEncounter == 14 ||
         randomEncounter == 16 ||randomEncounter == 18){
           console.log("no encounter this time");
+          
         }
       }
-  
+    }
 
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-//     var button = document.getElementById('attack-button');
-  
-//   button.onclick = function() {
-//     var result = dice.roll();
-//     printNumber(result);
-//   };
-  
-
-// const attack = () => {
-//   let attackButton =document.getElementById ("attack-button");
-//   let playerAttack = Math.floor (Math.random() * player.power);
-//   let gameMessage= document.getElementById ("game-message");
-//   opponent.health -= playerAttack;
-//   printToScreen();
-  
-//   attackButton.disabled = true;
-      
-//         gameMessage.innerText="opponent is about to strike!" 
-//         setTimeout(() => {
-//           let opponentAttack = Math.floor(Math.random() * opponent.power);
-//           player.health -= opponentAttack; 
-//           printToScreen();
-//           attackButton.disabled= false 
-//         },1000);
-
-//     }
-// const printToScreen = () => {
-//   document.getElementById ('opponent-health').innerText =
-//   opponent.health;
-
-//   document.getElementById ('player-health').innerText =
-//   player.health;
-
-// }
-// printToScreen
+function display(player1, currentPosition){
+  console.log("current Player Health" + " " + player1.health);
+  console.log("Current Position" + " " + currentPosition);
+}
 
 
+game();
